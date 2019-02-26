3b87904f6b3304d86200244f80c49cfac284f7ea
Peter Bynum <pkpbynum@gmail.com>
2019-02-25 19:47:16 -0500

    Facebook analyzer

--- facebook-message-analyzer.py
+++ facebook-message-analyzer.py
@@ -8,6 +8,7 @@ CURRENT_DIRECTORY = os.getcwd()
 NUMBER_TO_ANALYZE = 10000
 MESSAGE_THRESHOLD = 100
 
+
 def get_json_data(chat):
     try:
         json_location = CURRENT_DIRECTORY + "/messages/inbox/" + chat + "/message.json"
@@ -15,7 +16,9 @@ def get_json_data(chat):
             json_data = json.load(json_file)
             return json_data
     except IOError:
-        pass # some things the directory aren't messages (DS_Store, stickers_used, etc.)
+        # some things the directory aren't messages (DS_Store, stickers_used, etc.)
+        pass
+
 
 chats = os.listdir(CURRENT_DIRECTORY + "/messages/inbox")[:NUMBER_TO_ANALYZE]
 sorted_chats = []
@@ -23,6 +26,9 @@ final_data_messages = {}
 final_data_times = {}
 final_data_words = {}
 invalid_message_count = 0
+message_times = []
+my_message_times = []
+most_used_words_dict = {}
 
 print('Analyzing ' + str(min(NUMBER_TO_ANALYZE, len(chats))) + ' chats...')
 
@@ -34,7 +40,7 @@ for chat in chats:
             sorted_chats.append((len(messages), chat, messages))
 
 sorted_chats.sort(reverse=True)
-
+print(len(sorted_chats))
 print('Finished processing chats...')
 
 for i, (messages, chat, messages) in enumerate(sorted_chats):
@@ -48,35 +54,66 @@ for i, (messages, chat, messages) in enumerate(sorted_chats):
         try:
             name = message["sender_name"]
             time = message["timestamp_ms"]
-            message_content = message["content"]
+            message_content = message["content"].split()
 
             number_messages[name] = number_messages.get(name, 0)
             number_messages[name] += 1
 
             person_to_times[name] = person_to_times.get(name, [])
-            person_to_times[name].append(datetime.datetime.fromtimestamp(time/1000.0))
+            t = datetime.datetime.fromtimestamp(time/1000.0)
+            person_to_times[name].append(t)
+            message_times.append(t.hour)
+
+            for word in message_content:
+                most_used_words_dict[word] = most_used_words_dict.get(word, 0)
+                most_used_words_dict[word] += 1
 
             number_words[name] = number_words.get(name, [])
-            number_words[name].append(len(message_content.split()))
+            number_words[name].append(len(message_content))
         except KeyError:
             # happens for special cases like users who deactivated, unfriended, blocked
             invalid_message_count += 1
-    
+
     final_data_messages[i] = number_messages
     final_data_times[i] = person_to_times
     final_data_words[i] = number_words
 
+most_used_words = []
+for word, occ in most_used_words_dict.items():
+    if occ > 200:
+        most_used_words.append((word, occ))
+
+
+def sorter(el):
+    return el[1]
+
+
+most_used_words.sort(key=sorter, reverse=True)
+most_used_words = most_used_words[:50]
+
+xWords = []
+wordFreq = []
+
+for tpl in most_used_words:
+    xWords.append(tpl[0])
+    wordFreq.append(tpl[1])
+
+
 print('Found ' + str(invalid_message_count) + ' invalid messages...')
-print('Found ' + str(len(sorted_chats)) + ' chats with ' + str(MESSAGE_THRESHOLD) + ' messages or more')
+print('Found ' + str(len(sorted_chats)) + ' chats with ' +
+      str(MESSAGE_THRESHOLD) + ' messages or more')
+
 
 def plot_num_messages(chat_number):
     plotted_data = final_data_messages[chat_number]
     X = np.arange(len(plotted_data))
-    pl.bar(X, list(plotted_data.values()), align='center', width=0.5, color = 'r', bottom = 0.3)
-    pl.xticks(X, plotted_data.keys(), rotation = 90)
+    pl.bar(X, list(plotted_data.values()), align='center',
+           width=0.5, color='r', bottom=0.3)
+    pl.xticks(X, plotted_data.keys(), rotation=90)
     pl.title('Number of Messages Sent')
     pl.tight_layout()
     pl.show()
+
 
 def plot_histogram_time(chat_number):
     person_to_times = final_data_times[chat_number]
@@ -84,13 +121,15 @@ def plot_histogram_time(chat_number):
     pl.ylabel('Number of Messages')
     pl.title('# of Messages Over Time')
     colors = ['b', 'r', 'c', 'm', 'y', 'k', 'w', 'g']
-    for i , person in enumerate(person_to_times):
+    for i, person in enumerate(person_to_times):
         plotted_data = person_to_times[person]
-        pl.hist(plotted_data, 100, alpha=0.3, label=person, facecolor=colors[i % len(colors)])
+        pl.hist(plotted_data, 100, alpha=0.3, label=person,
+                facecolor=colors[i % len(colors)])
     pl.legend()
     pl.xticks(rotation=90)
     pl.tight_layout()
     pl.show()
+
 
 def plot_histogram_words(chat_number):
     temp = {}
@@ -98,15 +137,38 @@ def plot_histogram_words(chat_number):
         temp[person] = np.average(final_data_words[chat_number][person])
     plotted_data = temp
     X = np.arange(len(plotted_data))
-    pl.bar(X, list(plotted_data.values()), align='center', width=0.5, color = 'r', bottom = 0.3)
-    pl.xticks(X, plotted_data.keys(), rotation = 90)
+    pl.bar(X, list(plotted_data.values()), align='center',
+           width=0.5, color='r', bottom=0.3)
+    pl.xticks(X, plotted_data.keys(), rotation=90)
     pl.title('Average Word Count')
     pl.tight_layout()
     pl.show()
 
-def plot(chat_number):
-    plot_num_messages(chat_number)
-    plot_histogram_time(chat_number)
-    plot_histogram_words(chat_number)
 
-plot(4)
+def plot_word_occurence(xs, words):
+    X = np.arange(len(xs))
+    pl.bar(X, xs, align='center',
+           width=0.5, color='r', bottom=0.3)
+    pl.xticks(X, words, rotation=90)
+    pl.title('Frequency of Top 100 words')
+    pl.tight_layout()
+    pl.show()
+
+
+def plot_message_times(data, title):
+    pl.xlabel('Time (hr)')
+    pl.ylabel('Number of Messages')
+    pl.title(title)
+    pl.hist(data, 24, alpha=0.3, facecolor=(
+        1, 0, 0, 0.5), histtype='stepfilled')
+    pl.legend()
+    pl.tight_layout()
+    pl.show()
+
+
+def plot():
+    plot_message_times(message_times, "# of Messages Over 24 Hours")
+    plot_word_occurence(wordFreq, xWords)
+
+
+plot()
